3972a9b2fe8cff8c3e399ddb05359239c44050e2
its-pushpaks-world <115705491+its-pushpaks-world@users.noreply.github.com>
2025-05-30 17:02:44 +0530

    Update lstm_model.py

--- lstm_model.py
+++ lstm_model.py
@@ -1,7 +1,7 @@
 
 import numpy as np
 import pandas as pd
-from keras.models import Sequential
+from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import LSTM, Dense, Dropout
 from sklearn.preprocessing import MinMaxScaler
 
